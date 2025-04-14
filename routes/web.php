aa0efacb65c9a2e803953b9fbf94c58268c09517
Mehak patel <mehakpatelattitude03@gmail.com>
2025-04-14 18:55:28 +0530

    start with breeze

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductsController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+Route::get('/', [ProductsController::class, 'index'])->name('product.index');
+
+Route::prefix('products')->group(function(){
+    Route::post('/save-item', [ProductsController::class, 'store'])->name('product.store');
+    Route::get('/{id}/edit',[ProductsController::class, 'edit'])->name('product.edit');
+    Route::post('/{id}/update', [ProductsController::class, 'update'])->name('product.update');
+    Route::delete('/delete/{id}', [ProductsController::class, 'delete'])->name('product.delete');
+});
+
+require __DIR__.'/auth.php';
